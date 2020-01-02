4c3c6cf105c8d0db73ca12e11bdc67fd718b4709
Rene Ismael <Ismael_br7@hotmail.com>
2020-01-01 23:44:39 -0600

    Test Footer and redux

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,9 +3,14 @@ import { mount } from 'enzyme';
 
 import Footer from '../../components/Footer';
 
+//Mocks funciones que simulan acciones que tendria que hacer nuestra aplicacion
+
 describe('<Footer />', () => {
+  const footer = mount(<Footer />);
   test('Render del componente Footer', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+  test('Render del titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
